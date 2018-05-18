8c7f09403dd95ac4e54b2952985b316e2aa4f41d
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-05-18 21:46:48 +0200

    test: html button attribute

--- tests/ListBox.elm
+++ tests/ListBox.elm
@@ -3,19 +3,22 @@ module ListBox exposing (..)
 import Expect
 import Helpers
 import Html
+import Html.Attributes as Html
+import Html.Styled.Attributes as H
 import Test as T exposing (Test)
 import Test.Html.Query as Q
 import Test.Html.Selector as S
 import Widgets.ListBox as Widgets
-import Widgets.ListBox.Elements as Elements
+import Widgets.ListBox.Attributes as ListBox
+import Widgets.ListBox.Elements as ListBox
 
 
-listBox : Test
-listBox =
-    T.describe "ListBox Widget"
+elements : Test
+elements =
+    T.describe "ListBox Widget custom nodes"
         [ T.test "Adds button children nodes" <|
             \() ->
-                Widgets.listBox { id = "id" } [] [ Elements.textButton "foobar" ]
+                Widgets.listBox { id = "id" } [] [ ListBox.textButton "foobar" ]
                     |> Helpers.fromStyledHtml
                     |> Q.find [ S.tag "button" ]
                     |> Q.contains [ Html.text "foobar" ]
@@ -23,8 +26,8 @@ listBox =
             \() ->
                 Widgets.listBox { id = "id" }
                     []
-                    [ Elements.textOption "first"
-                    , Elements.textOption "second"
+                    [ ListBox.textOption "first"
+                    , ListBox.textOption "second"
                     ]
                     |> Helpers.fromStyledHtml
                     |> Q.findAll [ S.tag "li" ]
@@ -32,4 +35,21 @@ listBox =
                         [ Q.first >> Q.contains [ Html.text "first" ]
                         , Q.index 1 >> Q.contains [ Html.text "second" ]
                         ]
+        ]
+
+
+attributes : Test
+attributes =
+    T.describe "ListBox Widgets attributes"
+        [ T.test "`html ListBox.button` adds custom attributes to the button" <|
+            \() ->
+                Widgets.listBox { id = "id" }
+                    [ ListBox.html ListBox.button
+                        [ H.required True
+                        ]
+                    ]
+                    []
+                    |> Helpers.fromStyledHtml
+                    |> Q.find [ S.tag "button" ]
+                    |> Q.has [ S.attribute <| Html.required True ]
         ]
