f3a3f01d356e5644aa50a5f97e62dffcdb177874
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-05-20 00:39:14 +0200

    docs: set and active option in ListBox example

--- examples/ListBox.elm
+++ examples/ListBox.elm
@@ -12,7 +12,7 @@ view : Model -> Html msg
 view model =
     Widgets.listBox { id = "listbox", description = "A listbox example" }
         []
-        [ ListBox.textOption { selected = False, text = "one", id = "1" }
+        [ ListBox.textOption { selected = True, text = "one", id = "1" }
         , ListBox.textOption { selected = False, text = "two", id = "2" }
         , ListBox.textOption { selected = False, text = "three", id = "3" }
         , ListBox.textOption { selected = False, text = "over 9000", id = "9001" }
