4f3f9117ebb2a75f7283f6d790f2bf3d1dc6bd54
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-05-19 09:53:32 +0200

    test: ListBox description

--- tests/ListBox.elm
+++ tests/ListBox.elm
@@ -4,6 +4,7 @@ import Expect
 import Helpers
 import Html
 import Html.Attributes as Html
+import Html.Styled as H
 import Html.Styled.Attributes as H
 import Test as T exposing (Test)
 import Test.Html.Query as Q
@@ -19,21 +20,45 @@ elements =
     T.describe "ListBox Widget elements"
         [ T.test "Wrapper element has the provided id" <|
             \() ->
-                Widgets.listBox { id = "foobar" } [] []
+                Widgets.listBox { id = "foobar", description = "desc" } [] []
                     |> Helpers.fromStyledHtml
                     |> Q.has [ S.id "foobar" ]
         , T.test "Button element has the provided id" <|
             \() ->
-                Widgets.listBox { id = "foobar" } [] []
+                Widgets.listBox { id = "foobar", description = "desc" } [] []
                     |> Helpers.fromStyledHtml
                     |> Q.find [ S.tag "button" ]
                     |> Q.has [ S.id <| ListBox.id "foobar" ListBox.Button ]
         , T.test "List element has the provided id" <|
             \() ->
-                Widgets.listBox { id = "foobar" } [] []
+                Widgets.listBox { id = "foobar", description = "desc" } [] []
                     |> Helpers.fromStyledHtml
                     |> Q.find [ S.tag "ul" ]
                     |> Q.has [ S.id <| ListBox.id "foobar" ListBox.List ]
+        , T.test "Button sets aria haspopup attribute" <|
+            \() ->
+                Widgets.listBox { id = "foobar", description = "desc" } [] []
+                    |> Helpers.fromStyledHtml
+                    |> Q.find [ S.tag "button" ]
+                    |> Q.has [ S.attribute <| Html.attribute "aria-haspopup" "dialog" ]
+        , T.test "Button sets aria label attribute" <|
+            \() ->
+                Widgets.listBox { id = "id", description = "foobar" } [] []
+                    |> Helpers.fromStyledHtml
+                    |> Q.find [ S.tag "button" ]
+                    |> Q.has [ S.attribute <| Html.attribute "aria-label" "foobar" ]
+        , T.test "List sets aria label attribute" <|
+            \() ->
+                Widgets.listBox { id = "id", description = "foobar" } [] []
+                    |> Helpers.fromStyledHtml
+                    |> Q.find [ S.tag "ul" ]
+                    |> Q.has [ S.attribute <| Html.attribute "aria-label" "foobar" ]
+        , T.test "List sets aria role to listbox" <|
+            \() ->
+                Widgets.listBox { id = "foobar", description = "desc" } [] []
+                    |> Helpers.fromStyledHtml
+                    |> Q.find [ S.tag "ul" ]
+                    |> Q.has [ S.attribute <| Html.attribute "role" "listbox" ]
         ]
 
 
@@ -42,7 +67,7 @@ attributes =
     T.describe "ListBox Widget attributes"
         [ T.test "`html ListBox.button` adds custom attributes to the button" <|
             \() ->
-                Widgets.listBox { id = "id" }
+                Widgets.listBox { id = "id", description = "desc" }
                     [ ListBox.html ListBox.button
                         [ H.required True
                         ]
@@ -51,6 +76,40 @@ attributes =
                     |> Helpers.fromStyledHtml
                     |> Q.find [ S.tag "button" ]
                     |> Q.has [ S.attribute <| Html.required True ]
+        , T.test "`html ListBox.description` adds custom attributes to the description" <|
+            \() ->
+                Widgets.listBox { id = "id", description = "foobar" }
+                    [ ListBox.descriptionLabel
+                    , ListBox.html ListBox.description
+                        [ H.required True
+                        ]
+                    ]
+                    []
+                    |> Helpers.fromStyledHtml
+                    |> Q.find [ S.id <| ListBox.id "id" ListBox.Description ]
+                    |> Q.has [ S.attribute <| Html.required True ]
+        , T.test "descriptionLabel attribute sets aria-labeledby on button and list" <|
+            \() ->
+                Widgets.listBox { id = "id", description = "foobar" }
+                    [ ListBox.descriptionLabel
+                    ]
+                    []
+                    |> Helpers.fromStyledHtml
+                    |> Expect.all
+                        [ Q.find [ S.tag "button" ] >> Q.hasNot [ S.attribute <| Html.attribute "aria-label" "foobar" ]
+                        , Q.find [ S.tag "button" ] >> Q.has [ S.attribute <| Html.attribute "aria-labelledby" <| ListBox.id "id" ListBox.Description ]
+                        , Q.find [ S.tag "ul" ] >> Q.hasNot [ S.attribute <| Html.attribute "aria-label" "foobar" ]
+                        , Q.find [ S.tag "ul" ] >> Q.has [ S.attribute <| Html.attribute "aria-labelledby" <| ListBox.id "id" ListBox.Description ]
+                        ]
+        , T.test "descriptionLabel attribute shows the description" <|
+            \() ->
+                Widgets.listBox { id = "id", description = "foobar" }
+                    [ ListBox.descriptionLabel
+                    ]
+                    []
+                    |> Helpers.fromStyledHtml
+                    |> Q.find [ S.id <| ListBox.id "id" ListBox.Description ]
+                    |> Q.contains [ Html.text "foobar" ]
         ]
 
 
@@ -59,13 +118,22 @@ nodes =
     T.describe "ListBox Widget custom nodes"
         [ T.test "Adds button children nodes" <|
             \() ->
-                Widgets.listBox { id = "id" } [] [ ListBox.textButton "foobar" ]
+                Widgets.listBox { id = "id", description = "desc" } [] [ ListBox.textButton "foobar" ]
                     |> Helpers.fromStyledHtml
                     |> Q.find [ S.tag "button" ]
                     |> Q.contains [ Html.text "foobar" ]
+        , T.test "Adds description children nodes" <|
+            \() ->
+                Widgets.listBox { id = "id", description = "desc" }
+                    []
+                    [ ( ListBox.description, H.text "custom foobar" )
+                    ]
+                    |> Helpers.fromStyledHtml
+                    |> Q.find [ S.id <| ListBox.id "id" ListBox.Description ]
+                    |> Q.contains [ Html.text "custom foobar" ]
         , T.test "Adds options children nodes in order" <|
             \() ->
-                Widgets.listBox { id = "id" }
+                Widgets.listBox { id = "id", description = "desc" }
                     []
                     [ ListBox.textOption "first"
                     , ListBox.textOption "second"
