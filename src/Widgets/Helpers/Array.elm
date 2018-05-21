60f628f9fd45998d1a44a55ce9a7d573f507c32c
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-05-21 18:52:32 +0200

    chore: add getBounded Array helper

--- src/Widgets/Helpers/Array.elm
+++ src/Widgets/Helpers/Array.elm
@@ -1,6 +1,15 @@
-module Widgets.Helpers.Array exposing (singleton)
+module Widgets.Helpers.Array exposing (getBounded, singleton)
 
 import Array exposing (Array)
+
+
+getBounded : Int -> Array a -> Maybe a
+getBounded index array =
+    let
+        maxIndex =
+            Array.length array - 1
+    in
+        Array.get (max 0 (min index maxIndex)) array
 
 
 singleton : a -> Array a
