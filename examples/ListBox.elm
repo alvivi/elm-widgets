879682e13a950cede40b32b31b6afae7b3dddc19
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-05-21 18:57:32 +0200

    docs: use State in ListBox example

--- examples/ListBox.elm
+++ examples/ListBox.elm
@@ -1,22 +1,48 @@
 module ListBox exposing (main)
 
+import Css as C
+import Css.Colors as Color
 import Html.Styled as H exposing (Html)
+import Html.Styled.Attributes as H
+import KeywordList as K
 import Widgets.ListBox as Widgets
 import Widgets.ListBox.Elements as ListBox
+import Widgets.ListBox.State as ListBox
 
 
 -- View --
 
 
-view : Model -> Html msg
+view : Model -> Html Msg
 view model =
     Widgets.listBox { id = "listbox", description = "A listbox example" }
-        []
-        [ ListBox.textOption { selected = True, text = "one", id = "1" }
-        , ListBox.textOption { selected = False, text = "two", id = "2" }
-        , ListBox.textOption { selected = False, text = "three", id = "3" }
-        , ListBox.textOption { selected = False, text = "over 9000", id = "9001" }
-        ]
+        [ ListBox.attributes ListBoxMsg model.listBox ]
+        (List.map
+            (\{ id, text } ->
+                let
+                    isSelected =
+                        ListBox.selected model.listBox == Just id
+                in
+                    ( ListBox.option
+                        { selected = isSelected
+                        , text = text
+                        , id = id
+                        }
+                    , H.span
+                        [ H.css <|
+                            K.fromMany
+                                [ K.many
+                                    [ C.display C.inlineBlock
+                                    , C.width <| C.pct 100
+                                    ]
+                                , K.ifTrue isSelected <| C.backgroundColor Color.blue
+                                ]
+                        ]
+                        [ H.text text ]
+                    )
+            )
+            options
+        )
 
 
 
@@ -24,12 +50,14 @@ view model =
 
 
 type alias Model =
-    {}
+    { listBox : ListBox.Model }
 
 
 empty : Model
 empty =
-    {}
+    { listBox =
+        List.foldl ListBox.insertOption ListBox.empty options
+    }
 
 
 
@@ -37,22 +65,38 @@ empty =
 
 
 type Msg
-    = Noop
+    = ListBoxMsg ListBox.Msg
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    model
+    case msg of
+        ListBoxMsg subMsg ->
+            let
+                ( listBox, cmd ) =
+                    ListBox.update subMsg model.listBox
+            in
+                ( { model | listBox = listBox }, Cmd.map ListBoxMsg cmd )
 
 
 
 -- Initialization --
 
 
+options : List { id : String, text : String }
+options =
+    [ { text = "one", id = "1" }
+    , { text = "two", id = "2" }
+    , { text = "three", id = "3" }
+    , { text = "over 9000", id = "9001" }
+    ]
+
+
 main : Program Never Model Msg
 main =
-    H.beginnerProgram
-        { model = empty
+    H.program
+        { init = ( empty, Cmd.none )
         , view = view
         , update = update
+        , subscriptions = always Sub.none
         }
