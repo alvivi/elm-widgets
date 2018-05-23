e3d1a7d23ce41a0085e15c583156e64417340b90
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-05-23 08:05:54 +0200

    test: check aria attributes in MenuButton

--- tests/MenuButton.elm
+++ tests/MenuButton.elm
@@ -5,16 +5,48 @@ import Html.Attributes as Html
 import Test as T exposing (Test)
 import Test.Html.Query as Q
 import Test.Html.Selector as S
+import Widgets.ListBox.Elements as ListBox
 import Widgets.MenuButton as Widgets
 
 
 button : Test
 button =
     T.describe "Button element"
-        [ T.test "haspopup attribute is set to menu " <|
+        [ T.test "aria-haspopup attribute is set" <|
             \() ->
                 Widgets.menuButton { id = "foobar", description = "desc" } [] []
                     |> Helpers.fromStyledHtml
                     |> Q.find [ S.tag "button" ]
                     |> Q.has [ S.attribute <| Html.attribute "aria-haspopup" "menu" ]
+        , T.test "aira-controls attrtibues is set" <|
+            \() ->
+                Widgets.menuButton { id = "foobar", description = "desc" } [] []
+                    |> Helpers.fromStyledHtml
+                    |> Q.find [ S.tag "button" ]
+                    |> Q.has
+                        [ S.attribute <|
+                            Html.attribute "aria-controls" <|
+                                ListBox.id "foobar" ListBox.list
+                        ]
+        ]
+
+
+list : Test
+list =
+    T.describe "List element"
+        [ T.test "role is set to menu" <|
+            \() ->
+                Widgets.menuButton { id = "foobar", description = "desc" } [] []
+                    |> Helpers.fromStyledHtml
+                    |> Q.find [ S.tag "ul" ]
+                    |> Q.has [ S.attribute <| Html.attribute "role" "menu" ]
+        , T.test "role is set to none in option wrappers" <|
+            \() ->
+                Widgets.menuButton { id = "foobar", description = "desc" }
+                    []
+                    [ ListBox.textOption { selected = False, id = "1", text = "one" }
+                    ]
+                    |> Helpers.fromStyledHtml
+                    |> Q.find [ S.tag "li" ]
+                    |> Q.has [ S.attribute <| Html.attribute "role" "none" ]
         ]
