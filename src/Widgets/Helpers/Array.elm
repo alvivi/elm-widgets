27aba033baa5ebde4c61013cb207da2319c0c849
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-05-23 23:43:19 +0200

    feat: add first, last and getCycling Array helpers

--- src/Widgets/Helpers/Array.elm
+++ src/Widgets/Helpers/Array.elm
@@ -1,6 +1,18 @@
-module Widgets.Helpers.Array exposing (getBounded, singleton)
+module Widgets.Helpers.Array
+    exposing
+        ( first
+        , getBounded
+        , getCycling
+        , last
+        , singleton
+        )
 
 import Array exposing (Array)
+
+
+first : Array a -> Maybe a
+first =
+    Array.get 0
 
 
 getBounded : Int -> Array a -> Maybe a
@@ -10,6 +22,25 @@ getBounded index array =
             Array.length array - 1
     in
         Array.get (max 0 (min index maxIndex)) array
+
+
+getCycling : Int -> Array a -> Maybe a
+getCycling index array =
+    let
+        length =
+            Array.length array
+    in
+        if index < 0 then
+            Array.get (length - 1) array
+        else if index >= length then
+            Array.get 0 array
+        else
+            Array.get index array
+
+
+last : Array a -> Maybe a
+last array =
+    Array.get (Array.length array - 1) array
 
 
 singleton : a -> Array a
