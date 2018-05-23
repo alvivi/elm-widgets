ab3acbb9e07870f48634fe411aef9f028851ec42
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-05-23 23:47:35 +0200

    docs: update MenuButton example to use MenuButton State

--- examples/MenuButton.elm
+++ examples/MenuButton.elm
@@ -1,11 +1,14 @@
 module MenuButton exposing (main)
 
 import Css as C
+import Css.Colors as Color
 import Html.Styled as H exposing (Html)
 import Html.Styled.Attributes as H
 import KeywordList as K
 import Widgets.ListBox.Elements as ListBox
 import Widgets.MenuButton as Widgets
+import Widgets.MenuButton.Elements as MenuButton
+import Widgets.MenuButton.State as MenuButton
 
 
 -- View --
@@ -14,27 +17,23 @@ import Widgets.MenuButton as Widgets
 view : Model -> Html Msg
 view model =
     Widgets.menuButton { id = "menubutton", description = "a menu" }
-        []
+        [ MenuButton.attributes MenuButtonMsg model.menuButton ]
         (List.map
-            (\{ id, text } ->
-                ( ListBox.option
-                    { selected = False
-                    , text = text
-                    , id = id
-                    }
-                , H.span
-                    [ H.css <|
-                        K.fromMany
-                            [ K.many
-                                [ C.display C.inlineBlock
-                                , C.width <| C.pct 100
+            (\({ id, text, href } as linkData) ->
+                let
+                    isFocused =
+                        MenuButton.focused model.menuButton == Just id
+                in
+                    MenuButton.menuLink linkData
+                        [ H.css <|
+                            K.fromMany
+                                [ K.many
+                                    [ C.display C.inlineBlock
+                                    , C.width <| C.pct 100
+                                    ]
+                                , K.ifTrue isFocused <| C.backgroundColor Color.blue
                                 ]
-
-                            --, K.ifTrue isSelected <| C.backgroundColor Color.blue
-                            ]
-                    ]
-                    [ H.text text ]
-                )
+                        ]
             )
             options
         )
@@ -45,12 +44,19 @@ view model =
 
 
 type alias Model =
-    {}
+    { menuButton : MenuButton.Model }
 
 
 empty : Model
 empty =
-    {}
+    { menuButton =
+        List.foldl
+            (\{ id, text } ->
+                MenuButton.insertMenuItem { id = id, text = text }
+            )
+            (MenuButton.empty "menubutton")
+            options
+    }
 
 
 
@@ -58,24 +64,46 @@ empty =
 
 
 type Msg
-    = Noop
+    = MenuButtonMsg MenuButton.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        MenuButtonMsg subMsg ->
+            let
+                ( menuButton, cmd ) =
+                    MenuButton.update subMsg model.menuButton
+            in
+                ( { model | menuButton = menuButton }, Cmd.map MenuButtonMsg cmd )
 
 
 
 -- Initialization --
 
 
-options : List { id : String, text : String }
+options : List { id : String, text : String, href : String }
 options =
-    [ { text = "link one", id = "1" }
-    , { text = "link two", id = "2" }
-    , { text = "link three", id = "3" }
-    , { text = "link over 9000", id = "9001" }
+    [ { id = "wai-wai"
+      , text = "W3C Web Accessibility Initiative"
+      , href = "https://www.w3.org/standards/webdesign/accessibility"
+      }
+    , { id = "wai-rias"
+      , href = "https://www.w3.org/TR/wai-aria/"
+      , text = "Accessible Rich Internet Application Specification"
+      }
+    , { id = "wai-wap"
+      , href = "https://www.w3.org/TR/wai-aria-practices/"
+      , text = "WAI-ARIA Authoring Practices"
+      }
+    , { id = "wai-waim"
+      , href = "https://www.w3.org/TR/wai-aria-implementation/"
+      , text = "WAI-ARIA Implementation Guide"
+      }
+    , { id = "wai-aam"
+      , href = "https://www.w3.org/TR/accname-aam-1.1/"
+      , text = "Accessible Name and Description"
+      }
     ]
 
 
