4116359a08b69edb9dfb89cc4d3733aea6780d44
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-07-05 00:49:50 +0200

    feat: add intrinsicWidth css helper

--- src/Widgets/Helpers/Css.elm
+++ src/Widgets/Helpers/Css.elm
@@ -1,4 +1,4 @@
-module Widgets.Helpers.Css exposing (appearance)
+module Widgets.Helpers.Css exposing (appearance, intrinsicWidth)
 
 import Css as C exposing (Style)
 
@@ -9,4 +9,14 @@ appearance style =
         [ C.property "-moz-appearance" style
         , C.property "-webkit-appearance" style
         , C.property "appearance" style
+        ]
+
+
+intrinsicWidth : Style
+intrinsicWidth =
+    C.batch
+        [ C.property "width" "-moz-max-content"
+        , C.property "width" "-webkit-max-content"
+        , C.property "width" "-o-max-content"
+        , C.property "width" "max-content"
         ]
